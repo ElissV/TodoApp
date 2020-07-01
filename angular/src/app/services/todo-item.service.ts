8a33d2d2d417b72c6e157b51769752e372ac7199
8f4md201gv <elis.vassiltsova.v@gmail.com>
2020-07-01 16:00:53 +0300

    Todo item service added

--- angular/src/app/services/todo-item.service.ts
+++ angular/src/app/services/todo-item.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoItemService {
 
-  constructor() { }
+  baseUrl = "http://localhost:8080/api/todo";
+
+  constructor(private http: HttpClient) { }
+
+
+
 }
